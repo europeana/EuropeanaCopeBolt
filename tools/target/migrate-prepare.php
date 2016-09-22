bc546c55b38df31f112e80f920c800c93798daa0
Lodewijk Evers <lodewijk@twokings.nl>
2016-09-22 15:10:32 +0200

    update database migraiton queries

--- tools/target/migrate-prepare.php
+++ tools/target/migrate-prepare.php
@@ -24,33 +24,26 @@ $content_types = [
 ];
 
 foreach ($content_types as $ctype => $table) {
-  $query['relations_from'][] = "UPDATE bolt_relations r 
-  SET r.from_id = (
-    SELECT x.id 
-    FROM $table x 
-    JOIN bolt_relations y ON x.subsite_id = y.from_id
-    WHERE x.subsite = y.subsite
-    AND y.from_contenttype = '$ctype'
-  );\n";
+  $query['relations_from'][] = "UPDATE bolt_relations r, $table x SET r.from_id = x.id WHERE x.subsite_id = r.from_id AND x.subsite = r.subsite AND r.from_contenttype = '$ctype';\n";
 
-  $query['relations_to'][] = "UPDATE bolt_relations r 
-  SET r.to_id = (
-    SELECT x.id 
-    FROM $table x 
-    JOIN bolt_relations y ON x.subsite_id = y.from_id
-    WHERE x.subsite = y.subsite
-    AND y.to_contenttype = '$ctype'
-  );\n";
+  $query['relations_to'][] = "UPDATE bolt_relations r, $table x SET r.to_id = x.id WHERE x.subsite_id = r.to_id AND x.subsite = r.subsite AND r.to_contenttype = '$ctype';\n";
 }
+
 foreach ($content_types as $ctype => $table) {
-  $query['taxonomy'][] = "UPDATE bolt_taxonomy t 
-  SET t.content_id = (
-    SELECT x.id 
-    FROM $table x 
-    JOIN bolt_taxonomy y ON x.subsite_id = y.content_id
-    WHERE x.subsite = y.subsite
-    AND y.contenttype = '$ctype'
-  );\n";
+  $query['taxonomy'][] = "UPDATE bolt_taxonomy t, $table c SET t.content_id = c.id WHERE c.subsite_id = t.content_id AND c.subsite = t.subsite AND t.contenttype = '$ctype';\n";
+}
+
+// set username
+foreach ($content_types as $ctype => $table) {
+  $query['usernames'][] = "UPDATE $table c, bolt_users y SET c.username = y.username WHERE y.id = c.ownerid AND c.subsite = y.subsite;\n";
+}
+
+// remove duplicate users by username
+$query['user_dedupe'][] = "DELETE FROM bolt_users WHERE id IN ( SELECT u.id FROM bolt_users u, bolt_users u2 WHERE u.id <> u2.id AND u.username = u2.username AND u.id > u2.id );\n";
+
+// set userid
+foreach ($content_types as $ctype => $table) {
+  $query['user_id'][] = "UPDATE $table c, bolt_users y SET c.ownerid = y.id WHERE c.username = y.username;\n";
 }
 
 $query['cleanup'][] = "ALTER TABLE bolt_relations DROP subsite;\n";
@@ -59,7 +52,7 @@ $query['cleanup_id'][] = "ALTER TABLE bolt_relations DROP subsite_id;\n";
 $query['cleanup_id'][] = "ALTER TABLE bolt_taxonomy DROP subsite_id;\n";
 
 foreach ($content_types as $ctype => $table) {
-    $query['cleanup_id'][] = "ALTER TABLE $table DROP subsite_id;\n"; 
+    $query['cleanup_id'][] = "ALTER TABLE $table DROP subsite_id;\n";
 }
 
 foreach ($query as $group => $queries) {
