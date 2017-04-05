05c92ee58330e6af382fd3466d7a59201c4aff9e
Lodewijk Evers <lodewijk@twokings.nl>
2017-04-05 12:19:11 +0200

    sync extension js with theme js

--- extensions/local/europeana/viewblocks/web/viewblocks.backend.js
+++ extensions/local/europeana/viewblocks/web/viewblocks.backend.js
@@ -1,113 +1,138 @@
 jQuery.fn.extend(
     {
         loadViewBlock: function() {
-            // the current textarea
-            var currentkey = $(this).attr('id');
-            var currentname = $(this).attr('name');
+            console.log('initializing viewblock', $(this));
+            $(this).addClass('viewblocks');
 
-            // the parent of the current textarea - to define the context
-            var datasource = $(this).parent();
+            // prepare display according to current settings
+            $(this)
+                .templateSwitcher()
+                .orderSwitcher()
+                .sourceSwitcher();
 
-            // get the values for the selectboxes
-            var templates = $(datasource).data('templates')?$(datasource).data('templates'):{};
-            var sources = $(datasource).data('sources')?$(datasource).data('sources'):{};
-            var ordering = $(datasource).data('ordering')?$(datasource).data('ordering'):{};
-
-            //console.log('intitializing viewblocks field', currentkey, templates, sources, ordering);
-            console.log('intitializing viewblocks field', currentkey);
-
-            var templatesselect = $('<select>')
-                .attr(templates)
-                .attr('data-for', currentname)
-                .addClass(templates.class).addClass('col-sm-9 form-control');
-            for (var templatekey in templates.values) {
-                templatesselect.append($('<option>').attr('value', templatekey).text(templates.values[templatekey]));
-            }
-            $(templatesselect).changeViewBlockHandler();
-
-            var sourcesselect = $('<select>')
-                .attr(sources)
-                .attr('data-for', currentname)
-                .addClass(sources.class).addClass('col-sm-9 form-control');
-            for (var sourcekey in sources.values) {
-                sourcesselect.append($('<option>').attr('value', sourcekey).text(sources.values[sourcekey]));
-            }
-            $(sourcesselect).changeViewBlockHandler();
-
-            var orderingselect = $('<select>')
-                .attr(ordering)
-                .attr('data-for', currentname)
-                .addClass(ordering.class).addClass('col-sm-9 form-control');
-            for (var orderkey in ordering.values) {
-                orderingselect.append($('<option>').attr('value', orderkey).text(ordering.values[orderkey]));
-            }
-            $(orderingselect).changeViewBlockHandler();
-
-            // add the selectboxes to the document
-            $(datasource).prepend(orderingselect).prepend($('<label>')
-                .text(ordering.label)
-                .addClass('col-sm-3 control-label'));
-            $(datasource).prepend(sourcesselect).prepend($('<label>')
-                .text(sources.label)
-                .addClass('col-sm-3 control-label'));
-            $(datasource).prepend(templatesselect).prepend($('<label>')
-                .text(templates.label)
-                .addClass('col-sm-3 control-label'));
+            // add change handlers to block
+            $(this).on('change', this.callChanges);
 
             return this;
         },
-        changeViewBlockHandler: function() {
-            $(this).on('change', function() {
-                var currentvalue = $(this).val();
-                var currentkey = $(this).attr('name');
-                var target = $(this).attr('data-for');
-                // find the textarea with this name within the current parent
-                var field = $(this).parents().find('textarea[name="' + target + '"]');
-                // console.log('the changed element', currentkey, $(field).attr('name'));
+        callChanges: function() {
+            console.log('changes called', $(this));
+            $(this)
+                .templateSwitcher()
+                .orderSwitcher()
+                .sourceSwitcher();
+        },
+        templateSwitcher: function() {
+            var viewblock = $(this);
+            var templateselect = viewblock.find('select[name*="templates"]');
+            var newvalue = templateselect.val();
+            viewblock.data('templatevalue', newvalue);
+            console.log('templatesSwitcher triggered', viewblock.data('templatevalue'));
+            return viewblock;
+        },
+        orderSwitcher: function() {
+            var viewblock = $(this);
+            var orderselect = viewblock.find('select[name*="ordering"]');
+            var templatevalue = viewblock.data('templatevalue');
+            //console.log('templatevalue', templatevalue);
+            if(templatevalue == 'body') {
+                orderselect.parents('.repeater-field').hide();
+            } else {
+                orderselect.parents('.repeater-field').show();
+            }
+            var newvalue = orderselect.val();
+            viewblock.data('ordervalue', newvalue);
+            console.log('orderSwitcher triggered', viewblock.data('ordervalue'));
+            return viewblock;
+        },
+        sourceSwitcher: function() {
+            var viewblock = $(this);
+            var sourceselect = viewblock.find('select[name*="sources"]');
+            var newvalue = sourceselect.val();
+            viewblock.data('sourcevalue', newvalue);
+            //console.log('sourceSwitcher triggered', newvalue, sourceselect);
+            var parentblock = sourceselect.parents('.panel.viewblocks');
+            var selectedamount = parentblock.find('input[name*="override_amount"]');
+            var selectedct = parentblock.find('select[name*="selected"]');
+            var amount =  selectedct.length;
+            //console.log('selected content types', sourceselect, newvalue, parentblock, selectedct, amount);
 
-                var key, subkey = currentkey;
-                var match = currentkey.replace(']', '').split('[');
-                if(match) {
-                    key = match[1] ? match[1] : null;
-                    subkey = match[2] ? match[2] : null;
+            var templatevalue = viewblock.data('templatevalue');
+            if(templatevalue == 'body') {
+                sourceselect.parents('.repeater-field').hide();
+                selectedamount.parents('.repeater-field').hide();
+            } else {
+                sourceselect.parents('.repeater-field').show();
+            }
+
+            var orderingvalue = viewblock.data('ordervalue');
+            if(orderingvalue == 'specified') {
+                selectedamount.parents('.repeater-field').hide();
+            } else {
+                selectedamount.parents('.repeater-field').show();
+            }
+
+            // loop through all options to hide the source selector
+            for (var ct = 0; ct < amount; ct++) {
+                var currentct = $(selectedct[ct]);
+                var ctname = currentct.attr('name') ? currentct.attr('name') : false;
+                var parentfieldset = $(currentct.parents('.repeater-field'));
+                parentfieldset.hide();
+                if (ctname.indexOf(newvalue) != -1
+                    && templatevalue !== 'body'
+                    && orderingvalue == 'specified') {
+                    console.log('enable', newvalue, 'ctname', ctname);
+                    parentfieldset.show();
                 }
-                console.log('field: ', target, ' key:', key, subkey, ' -  set to:', currentvalue);
-
-                var newvalue, oldvalue;
-                oldvalue = $(field).val();
-                if(oldvalue.trim() != '') {
-                    //console.log('not empty', oldvalue);
-                    newvalue = JSON.parse(oldvalue);
-                } else {
-                    //console.log('empty', oldvalue);
-                    newvalue = {
-                        'templates': null,
-                        'sources': null,
-                        'ordering': null,
-                    };
-                }
-                newvalue[key] = currentvalue;
-                //console.log('newvalue', newvalue);
-
-                $(field).val(JSON.stringify(newvalue));
-            });
+            }
+            return viewblock;
+        },
+        onAvailable: function(fn){
+            var sel = this.selector;
+            var timer;
+            var self = this;
+            if (this.length > 0) {
+                fn.call(this);
+            }
+            else {
+                timer = setInterval(function(){
+                    if ($(sel).length > 0) {
+                        fn.call($(sel));
+                        clearInterval(timer);
+                    }
+                },50);
+            }
         }
     }
 );
 
 
 jQuery(document).ready(function($) {
+    console.log('viewblocks js start');
+
+    $('.repeater-slot .repeater-group').onAvailable(
+        function() {
+            //console.log('initializing viewblock for initial fields');
+            $(this).each(function() {
+                console.log('initializing viewblocks for new field', $(this));
+                $(this).loadViewBlock();
+            });
+        }
+    );
+    $('.repeater-add button').on('click', function() {
+        //console.log('a repeater is added');
+        setTimeout(
+            function() {
+                $('.repeater-slot .repeater-group:not(.viewblocks)').onAvailable(
+                    function() {
+                        console.log('initializing viewblocks for new fields', $(this));
+                        $(this).loadViewBlock();
+                    }
+                );
+            },
+            200
+        );
+    });
+
     console.log('viewblocks js loaded');
-
-    $('.viewblocks-field').each( function() {
-        console.log('loading each viewblock for normal field', $(this).attr('name'));
-        $(this).loadViewBlock();
-    } );
-
-    // $('.repeater-field .viewblocks .viewblocks-field').each(function() {
-    //     console.log('loading repeater viewblock for repeater field', $(this).attr('name'));
-    //     $(this).loadViewBlock();
-    // } );
-
-
 });
