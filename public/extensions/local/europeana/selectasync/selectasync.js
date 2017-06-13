931579f74c4e7d63acb39b5b05472a613f1aadb6
Lodewijk Evers <lodewijk@twokings.nl>
2017-06-13 16:37:31 +0200

    trying to get selectize under control

--- public/extensions/local/europeana/selectasync/selectasync.js
+++ public/extensions/local/europeana/selectasync/selectasync.js
@@ -13,60 +13,93 @@ jQuery(document).ready(function($) {
         $(this).data('contentkey',
             dataclasses.find(findKey).split('-').pop());
 
-        console.log('selectasync dataclasses', $(this).data());
+        //console.log('selectasync dataclasses', $(this).data());
 
         var datakeys = $(this).val();
         datavalues = [];
         if(datakeys) {
             datakeys = JSON.parse(datakeys);
+            console.log('datakeys', datakeys);
             datakeys.forEach(function(e) {
                 datavalues.push({ id: e, text: 'item '+ e});
             });
+            $.ajax({
+                url: '/admin/selectasync/load/',
+                type: 'GET',
+                dataType: "json",
+                data: {
+                    ids: datakeys,
+                    fields: this.settings.contentfields.join(',')
+                },
+                error: function(data) {
+                    console.log('error', data);
+                },
+                success: function(data) {
+                    console.log('result data', data.results, data);
+                }
+            });
             console.log('selectasync for ' + $(this).attr('name'), datakeys, datavalues);
         } else {
-            console.log('selectasync for ' + $(this).attr('name') + ' is empty');
+            // console.log('selectasync for ' + $(this).attr('name') + ' is empty');
         }
 
-        var options = { create: true, sortfield: 'text', options: datavalues, items: datakeys };
-
-        var newoptions = {
-            valueField: '/admin/selectasync/types/',
+        var options = {
+            valueField: '/admin/selectasync/',
             labelField: 'name',
             searchField: 'name',
             create: false,
             contenttype: $(this).data('contenttype'),
             contentfields: $(this).data('contentfields'),
             contentkey: $(this).data('contentkey'),
-            datavalues: datavalues,
-            datakeys: datakeys,
+            options: datavalues,
+            items: datakeys,
             render: {
+                item: function(item, escape) {
+                    console.log('render items', item, escape);
+                    return '<div>' +
+                        (item.id ? '<span class="id">' + escape(item.id) + '</span>' : '') +
+                        (item.title ? '<span class="title">' + escape(item.title) + '</span>' : '') +
+                        (item.status ? '<span class="status">' + escape(item.status) + '</span>' : '') +
+                        '</div>';
+                },
                 option: function(item, escape) {
-                    console.log('render results', item, escape);
-                    return item;
+                    console.log('render options', item, escape);
+                    return '<div>' +
+                        (item.id ? '<span class="id">' + escape(item.id) + '</span>' : '') +
+                        (item.title ? '<span class="title">' + escape(item.title) + '</span>' : '') +
+                        (item.status ? '<span class="status">' + escape(item.status) + '</span>' : '') +
+                        '</div>';
                 }
             },
             load: function(query, callback) {
                 if (!query.length) return callback();
-                console.log('load function', query, callback, this);
+                var config = this.settings;
+                //console.log('load function', query, config);
                 $.ajax({
-                    url: '/admin/selectasync/type/' + this.settings.contenttype,
+                    url: '/admin/selectasync/type/' + config.contenttype,
                     type: 'GET',
                     dataType: "json",
                     data: {
                         search: query,
-                        fields: this.settings.contentfields.join(',')
+                        fields: config.contentfields.join(',')
                     },
                     error: function() {
                         callback();
                     },
-                    success: function(result) {
-                        console.log(result);
+                    success: function(data) {
+                        //console.log('result data from load function - callback:', callback, 'results:', data.results[data.type], 'data:', data);
+                        var results = data.results[data.type];
+                        var items = results.map(function(x) {
+                            return { value: x.id, text: x.title };
+                        });
+                        //console.log('result items', items);
+                        callback(items);
                     }
                 });
             }
         };
-        console.log('selectize initialization', newoptions, $(this).data());
-        $(this).selectize(newoptions);
+        // console.log('selectize initialization', options, $(this).data());
+        $(this).selectize(options);
     });
 });
 
