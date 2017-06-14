224021744ba1a8bf0ae34007b35710d3ce2bff17
Lodewijk Evers <lodewijk@twokings.nl>
2017-06-14 11:46:58 +0200

    move stuff around in selectize - no workieworkie

--- public/extensions/local/europeana/selectasync/selectasync.js
+++ public/extensions/local/europeana/selectasync/selectasync.js
@@ -34,24 +34,25 @@ jQuery(document).ready(function($) {
                 'min-height': '2em'
             }).addClass('selectizer')
         );
-        $(this).hide();
+        //$(this).hide();
         // console.log('initialized selectasync:', newname);
         // console.log('selectasync parent:', $(this));
         // console.log('selectasync dataclasses:', $(this).data());
-    });
 
-    $('.selectizer').each(function() {
-        // console.log('selectasync element:', $(this).attr('class'), $(this));
-        var target = $('input[name="'+ $(this).data('target') + '"]');
-        var datakeys = $(target).val();
-        // console.log('selectasync element:', element, target, datakeys);
-        datavalues = [];
-        if(datakeys && isJson(datakeys)) {
-            datakeys = JSON.parse(datakeys);
-            console.log('datakeys from json', datakeys);
-            datakeys.forEach(function(e) {
-                datavalues.push({ id: e, title: 'item '+ e});
-            });
+        $('.selectizer').each(function() {
+            // console.log('selectasync element:', $(this).attr('class'), $(this));
+            var target = $('input[name="'+ $(this).data('target') + '"]');
+            var datakeys = $(target).val();
+            // console.log('selectasync element:', target, target.data(), datakeys);
+            datavalues = [];
+            if(datakeys && isJson(datakeys)) {
+                datakeys = JSON.parse(datakeys);
+            } else if(datakeys && !isJson(datakeys)) {
+                datakeys = datakeys.split(',');
+            } else {
+                // console.log('selectasync for ' + $(this).attr('name') + ' is empty');
+                datakeys = []
+            }
             // preload options
             $.ajax({
                 url: '/admin/selectasync/load/',
@@ -59,102 +60,83 @@ jQuery(document).ready(function($) {
                 dataType: "json",
                 data: {
                     ids: datakeys,
-                    fields: target.settings.contentfields.join(',')
+                    type: target.data('contenttype'),
+                    fields: target.data('contentfields').join(',')
                 },
                 error: function(data) {
                     console.log('error', data);
                 },
                 success: function(data) {
-                    data.results.forEach(function(current) {
-                        datavalues.push(current);
-                    });
-                    console.log('result data', data.results, data);
-                }
-            });
-            console.log('selectasync for ' + $(this).attr('name'), datakeys, datavalues);
-        } else if(datakeys && !isJson(datakeys)) {
-            datakeys = datakeys.split(',');
-            console.log('datakeys from string', datakeys);
-            $.ajax({
-                url: '/admin/selectasync/load/',
-                type: 'GET',
-                dataType: "json",
-                data: {
-                    ids: datakeys,
-                    fields: target.settings.contentfields.join(',')
-                },
-                error: function(data) {
-                    console.log('error', data);
-                },
-                success: function(data) {
-                    data.results.forEach(function(current) {
-                        datavalues.push(current);
-                    });
-                    console.log('result data', data.results, data);
-                }
-            });
-            console.log('selectasync for ' + $(this).attr('name'), datakeys, datavalues);
-        } else {
-            // console.log('selectasync for ' + $(this).attr('name') + ' is empty');
-        }
-        // console.log('target data', $(target).data());
-        var options = {
-            contentselector: $(target).attr('name'),
-            contenttype: $(target).data('contenttype'),
-            contentfields: $(target).data('contentfields'),
-            contentkey: $(target).data('contentkey'),
-            valuefield: 'id',
-            labelField: 'title',
-            searchField: ['title'],
-            delimiter: ',',
-            maxItems: null,
-            create: false,
-            options: datavalues,
-            items: datakeys,
-            render: {
-                item: function(item, escape) {
-                    console.log('render items', item, escape);
-                    return '<div class="item">' +
-                        (item.title ? '<span class="title">' + escape(item.title) + '</span>' : '') +
-                        (item.status ? '<span class="status">' + escape(item.status) + '</span>' : '') +
-                        '</div>';
-                },
-                option: function(item, escape) {
-                    console.log('render options', item, escape);
-                    return '<div class="option">' +
-                        (item.title ? '<span class="title">' + escape(item.title) + '</span>' : '-no title-') +
-                        (item.status ? '<span class="status">' + escape(item.status) + '</span>' : '') +
-                        '</div>';
-                }
-            },
-            load: function(query, callback) {
-                if (!query.length) return callback();
-                var settings = this.settings;
-                console.log('load ajax content for:', settings.contentselector, 'with search term:', query);
-                $.ajax({
-                    url: '/admin/selectasync/type/' + settings.contenttype,
-                    type: 'GET',
-                    dataType: "json",
-                    data: {
-                        search: query,
-                        fields: settings.contentfields.join(','),
-                        type: settings.contenttype
-                    },
-                    error: function() {
-                        callback();
-                    },
-                    success: function(data) {
-                        callback(data.results[data.type]);
+                    if (data.results[data.type]) {
+                        data.results[data.type].forEach(function(e) {
+                            datavalues.push(e);
+                        });
                     }
-                });
-            }
-        };
-        // console.log('selectize initialization', options, $(this).data());
-        $selectors.push($(this).selectize(options));
-        console.log('selectize initialization for:', $(this).attr('name'), $(target).attr('name'));
-    });
+                    console.log('result data', data.results[data.type], data, datavalues);
+                }
+            });
 
-    console.log('all selectors', $selectors);
+            // console.log('target data', $(target).data());
+            var selectizeoptions = {
+                contentselector: $(target).attr('name'),
+                contenttype: $(target).data('contenttype'),
+                contentfields: $(target).data('contentfields'),
+                contentkey: $(target).data('contentkey'),
+                valuefield: 'id',
+                labelField: 'title',
+                searchField: ['title'],
+                delimiter: ',',
+                maxItems: null,
+                create: false,
+                options: datavalues,
+                items: datakeys,
+                render: {
+                    item: function(item, escape) {
+                        console.log('render items', item, escape);
+                        return '<div class="item">' +
+                            (item.title ? '<span class="title">' + escape(item.title) + '</span>' : '') +
+                            (item.status ? '<span class="status">' + escape(item.status) + '</span>' : '') +
+                            '</div>';
+                    },
+                    option: function(item, escape) {
+                        console.log('render options', item, escape);
+                        return '<div class="option">' +
+                            (item.title ? '<span class="title">' + escape(item.title) + '</span>' : '-no title-') +
+                            (item.status ? '<span class="status">' + escape(item.status) + '</span>' : '') +
+                            '</div>';
+                    }
+                },
+                load: function(query, callback) {
+                    if (!query.length) return callback();
+                    var settings = this.settings;
+                    console.log('load ajax content for:', settings.contentselector, 'with search term:', query);
+                    $.ajax({
+                        url: '/admin/selectasync/type/' + settings.contenttype,
+                        type: 'GET',
+                        dataType: "json",
+                        data: {
+                            search: query,
+                            fields: settings.contentfields.join(','),
+                            type: settings.contenttype
+                        },
+                        error: function() {
+                            callback();
+                        },
+                        success: function(data) {
+                            callback(data.results[data.type]);
+                        }
+                    });
+                }
+            };
+
+            // console.log('selectize initialization', options, $(this).data());
+            $selectors.push($(this).selectize(selectizeoptions));
+
+            console.log('selectize initialization for:', $(this).attr('name'), $(target).attr('name'));
+        });
+
+        console.log('all selectors', $selectors);
+    });
 });
 
 function findCT(keys) {
