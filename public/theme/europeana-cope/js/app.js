16d28f976f3204e02d643f14974ded10639ed6c6
Anke Willems <anke@twokings.nl>
2017-07-19 16:56:45 +0200

    Adds image attribution to banners - option for js-hover. WIP

--- public/theme/europeana-cope/js/app.js
+++ public/theme/europeana-cope/js/app.js
@@ -268,6 +268,21 @@ $( document ).ready(function() {
         }
     })
 
+    /**
+    * Open en close image attribution
+    *
+    */
+
+    $('button.image-info').on('mouseenter', function(e){
+        $(this).siblings('article').addClass('expanded');
+        console.log('hover');
+    });
+
+    $('.license-attribution article').on('mouseleave', function(e){
+        $(this).removeClass('expanded');
+    });
+
+
 
     /**
     * Open en close filters
