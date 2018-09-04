41eff5d33e1f4ce918396759218ee72e211bd35f
Anke Willems <anke@twokings.nl>
2018-09-04 11:59:32 +0200

    Adds new EU logo and disclaimer - EP74

--- public/theme/europeana-cope/source/gulpfile.js
+++ public/theme/europeana-cope/source/gulpfile.js
@@ -31,21 +31,20 @@ var jsFiles = [
 
 var jsDest = '../js/';
 
-gulp.task('scripts', function() {
+gulp.task('scripts', gulp.series(function() {
     return gulp.src(jsFiles)
     .pipe(plugins.concat('scripts.min.js'))
     .pipe(gulp.dest(jsDest))
     .pipe(plugins.uglify())
     .pipe(gulp.dest(jsDest));
-});
+}));
 
-gulp.task('watch', function() {
-    gulp.watch('scss/**/*.scss', ['sass']);
-    gulp.watch('../*.css', ['autoprefixer']);
-    gulp.watch('../js/**/*.js', ['scripts']);
-});
+gulp.task('watch', gulp.series(function() {
+    gulp.watch('scss/**/*.scss', gulp.series('sass'));
+    gulp.watch('../js/**/*.js', gulp.series('scripts'));
+}));
 
-gulp.task('sass', function() {
+gulp.task('sass', gulp.series(function() {
     return gulp.src('scss/**/*.scss')
     .pipe(! isProduction ? plugins.sourcemaps.init() : gutil.noop())
     .pipe(plugins.sass({
@@ -58,8 +57,6 @@ gulp.task('sass', function() {
     .pipe(isProduction ? plugins.cleanCss() : gutil.noop())
     .pipe(! isProduction ?  plugins.sourcemaps.write() : gutil.noop())
     .pipe(gulp.dest('../css/'))
-});
+}));
 
-gulp.task('default', function(callback) {
-    runSequence('sass', 'watch', callback);
-});
+gulp.task('default', gulp.series('sass', 'watch'));
