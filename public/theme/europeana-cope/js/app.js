5894c51b8cf3f5aa37f2acf6e6bd4f0f21cc099f
Anke Willems <anke@twokings.nl>
2018-10-26 11:43:12 +0200

    Functioning cookiebar

--- public/theme/europeana-cope/js/app.js
+++ public/theme/europeana-cope/js/app.js
@@ -121,6 +121,22 @@ $( document ).ready(function() {
         }
     });
 
+    /**
+     * Cookiebar
+     */
+
+    var cookiebar = $('#cookiebar');
+    var hasCookie = getCookie('epro_cookieconsent');
+    
+    if (!hasCookie){
+        cookiebar.show();
+        cookiebar.find('button').on('click', function(){
+            cookiebar.slideUp('fast');
+            cookiebar.fadeOut();
+            //set cookie
+            setCookie('epro_cookieconsent','1',30);
+        });
+    }
 
     /**
     * Show 'back to top' button on scroll up
@@ -347,3 +363,27 @@ $( document ).ready(function() {
         }
     }
 });
+
+
+function setCookie(name,value,days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+function eraseCookie(name) {   
+    document.cookie = name+'=; Max-Age=-99999999;';  
+}
