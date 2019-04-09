9625d81018d4cd28a5e0a549e574c289d315b3af
Anke Willems <anke@twokings.nl>
2019-04-09 10:41:59 +0200

    Adds magnific Popup for pdf

--- public/theme/europeana-cope/source/js/splashpage_bizplan2019.js
+++ public/theme/europeana-cope/source/js/splashpage_bizplan2019.js
@@ -1,57 +1,66 @@
-// Splashpage BIZ PLAN 2019 (Annual Report)
 
-var article = $('.splashpage-bizplan2019 .wheel article');
-var section = $('.splashpage-bizplan2019 .wheel-section');
-var handle = $('.splashpage-bizplan2019 .wheel article .handle');
-var anchor = $('.splashpage-bizplan2019 .wheel article a');
-
-article
-    .on('mouseenter touchend', function(e){
-        $(this).addClass('highlight');
-        $(this).find('.center').show();
-
-        article.stop();
-        $(this).show();
-
-        article.not( $(this) ).find('.center').fadeOut({
-            duration: 200,
-            queue: false
-        });
-
-        article.not( $(this) ).removeClass('highlight');
-        $('#maincenter').hide();
-
-        e.preventDefault();
-        e.stopPropagation();
-        // return false;
-
-        // console.log('going in!');
-
-    })
-    .on('mouseleave', function(){  // not triggered on touch
-        $(this).removeClass('highlight');
-        $(this).find('.center').hide();
-        $('#maincenter').show();
-
-        article.stop().fadeIn({ duration: 0 });
-        // console.log('... and leaving.');
+$(document).ready(function() {
+    $('.iframe-popup').magnificPopup({
+        type: 'iframe'
     });
 
 
-anchor.on('click touchend', function(e) {
-    window.location = $(this).attr('href');
-    e.preventDefault();
-    e.stopPropagation();
-    return false;
-});
+    // Splashpage BIZ PLAN 2019 (Annual Report)
 
-//reset all als erbuiten geklikt
-section.on('click touchend',function(){
-    article.removeClass('highlight');
-    article.find('.center').hide();
-    $('#maincenter').show();
+    var article = $('.splashpage-bizplan2019 .wheel article');
+    var section = $('.splashpage-bizplan2019 .wheel-section');
+    var handle = $('.splashpage-bizplan2019 .wheel article .handle');
+    var anchor = $('.splashpage-bizplan2019 .wheel article a');
 
-    article.stop().fadeIn({ duration: 0 });
+    article
+        .on('mouseenter touchend', function(e){
+            $(this).addClass('highlight');
+            $(this).find('.center').show();
 
-    // console.log('reset all the things');
+            article.stop();
+            $(this).show();
+
+            article.not( $(this) ).find('.center').fadeOut({
+                duration: 200,
+                queue: false
+            });
+
+            article.not( $(this) ).removeClass('highlight');
+            $('#maincenter').hide();
+
+            e.preventDefault();
+            e.stopPropagation();
+            // return false;
+
+            // console.log('going in!');
+
+        })
+        .on('mouseleave', function(){  // not triggered on touch
+            $(this).removeClass('highlight');
+            $(this).find('.center').hide();
+            $('#maincenter').show();
+
+            article.stop().fadeIn({ duration: 0 });
+            // console.log('... and leaving.');
+        });
+
+
+    anchor.on('click touchend', function(e) {
+        window.location = $(this).attr('href');
+        e.preventDefault();
+        e.stopPropagation();
+        return false;
+    });
+
+    //reset all als erbuiten geklikt
+    section.on('click touchend',function(){
+        article.removeClass('highlight');
+        article.find('.center').hide();
+        $('#maincenter').show();
+
+        article.stop().fadeIn({ duration: 0 });
+
+        // console.log('reset all the things');
+    });
+
 });
