635516c9b46a7b948265e6064688f7e07ee633a4
Ivo Valchev <Ivo.V.Valchev@gmail.com>
2021-03-04 13:32:41 +0100

    Update accessibility.sh, package.json and readme

--- public/theme/europeana-cope/source/accessibility.sh
+++ public/theme/europeana-cope/source/accessibility.sh
@@ -1,6 +1,12 @@
 ## Make sure that we have one DEST_DIR across all npm scripts.
-export DEST_DIR=./reports/$(date "+%m-%Y")
+export DEST_DIR="/home/dropbox/Dropbox (Europeana Pro)/accessibility-reports/$(date \"+%m-%Y\")"
+export SUMMARY_PATH="${DEST_DIR}/summary.json"
 
+## Prepare the environment.
 npm run accessibility:lighthouse:prepare
+
+## Run the analyses
 npm run accessibility:lighthouse:analyse
+
+## Send emails
 npm run accessibility:lighthouse:send
