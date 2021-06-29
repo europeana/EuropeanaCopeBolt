a961f5d80ddc60f43dd47aaa83dafc7a04dbbafc
Dennis <dennis@twokings.nl>
2021-06-29 10:04:57 +0200

    Initial commit with klaro cookie consent implementation

---
+++ public/theme/europeana-cope/js/klaro-button.js
@@ -0,0 +1,29 @@
+jQuery(document).ready(function () {
+    $('.cn-decline, .cm-btn-success').on('click', function () {
+        $('#cookie-button').show('slow', function () {
+        })
+    })
+})
+
+function getCookie(){
+
+}
+
+
+$(document).on('click', '.cm-btn-accept-all', function () {
+    $('#cookie-button').show('slow', function () {
+
+    })
+})
+
+$(document).on('click', '.cn-decline', function () {
+    $('#cookie-button').show('slow', function () {
+
+    })
+})
+
+$(document).on('click', '.cm-btn-accept', function () {
+    $('#cookie-button').show('slow', function () {
+
+    })
+})
