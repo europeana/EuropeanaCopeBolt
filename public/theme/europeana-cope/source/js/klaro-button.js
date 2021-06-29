b342035a10fc9a66e8c8e6e7bd194ab87f4c61f5
Dennis <dennis@twokings.nl>
2021-06-29 15:52:36 +0200

    Formatted code

--- public/theme/europeana-cope/source/js/klaro-button.js
+++ public/theme/europeana-cope/source/js/klaro-button.js
@@ -1,33 +1,25 @@
 jQuery(document).ready(function () {
     $('.cn-decline, .cm-btn-success').on('click', function () {
-        $('#cookie-button').show('slow', function () {
-        })
+        $('#cookie-button').show('slow')
     })
 })
 
 function getCookie() {
     if (document.cookie.includes('klaro')) {
-        $('#cookie-button').show('slow', function () {
-        })
+        $('#cookie-button').show('slow')
     }
 }
 
 getCookie()
 
 $(document).on('click', '.cm-btn-accept-all', function () {
-    $('#cookie-button').show('slow', function () {
-
-    })
+    $('#cookie-button').show('slow')
 })
 
 $(document).on('click', '.cn-decline', function () {
-    $('#cookie-button').show('slow', function () {
-
-    })
+    $('#cookie-button').show('slow')
 })
 
 $(document).on('click', '.cm-btn-accept', function () {
-    $('#cookie-button').show('slow', function () {
-
-    })
+    $('#cookie-button').show('slow')
 })
